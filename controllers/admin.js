9d6a930660e43d0ba4b468e716a51c3e355ab966
MaxKingsbury-West <maxkwest99@gmail.com>
2021-03-30 21:47:55 +1100

    register admin

---
+++ controllers/admin.js
@@ -0,0 +1,27 @@
+const User = require('../models/user');
+
+module.exports.newAdmin = async (req, res) => {
+	try {
+		const { email, username, password } = req.body;
+		const user = new User({ email, username });
+		const registeredUser = await User.register({user, password, role: 'admin'});
+		req.login(registeredUser, (err) => {
+			if (err) return next(err);
+			req.flash('success', `Welcome ${username}`);
+			res.redirect('/');
+			Emails.welcome(email, username);
+		});
+	} catch (e) {
+		req.flash('error', e.message);
+		res.redirect('/register');
+	}
+
+};
+
+module.exports.renderIndex = (req, res) => {
+    res.render('admin/index');
+
+}
+module.exports.renderNewAdmin = (req, res) => {
+    res.render('admin/new')
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const { newAdmin, renderNewAdmin, renderIndex } = require('../controllers/admin');
 const { isLoggedIn } = require('../middleware/users');
 
-router.get('/', isLoggedIn, (req, res) => {
-	res.render('admin/index');
-});
+router.route('/')
+	.get(isLoggedIn, renderIndex)
+	.post(newAdmin)
+
+router.get('/new', renderNewAdmin)
+
+
 
 module.exports = router;
