e3e1384c468ea66d7e6c1168ac704c73b95a9322
Jacob Gad <jacob.gad@hotmail.com>
2021-03-31 00:42:35 +1100

    edit staff details

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const { newAdmin, renderNewAdmin, renderIndex } = require('../controllers/admin');
+const { newAdmin, renderNewAdmin, renderIndex, renderStaff, newStaff, deleteStaff, updateStaff } = require('../controllers/admin');
 const { isLoggedIn } = require('../middleware/users');
 
 router.route('/')
 	.get(isLoggedIn, renderIndex)
-	.post(newAdmin)
+	.post(newAdmin);
 
-router.get('/new', renderNewAdmin)
+router.get('/new', renderNewAdmin);
 
-
+router.route('/staff')
+	.get(renderStaff)
+	.post(newStaff)
+	.put(updateStaff)
+	.delete(deleteStaff);
 
 module.exports = router;
