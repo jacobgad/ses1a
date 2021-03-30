528af1e2eb623c19e996b5f0b1a5861c53dd3dc5
MaxKingsbury-West <maxkwest99@gmail.com>
2021-03-30 22:00:23 +1100

    revised admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,8 +3,8 @@ const User = require('../models/user');
 module.exports.newAdmin = async (req, res) => {
 	try {
 		const { email, username, password } = req.body;
-		const user = new User({ email, username });
-		const registeredUser = await User.register({user, password, role: 'admin'});
+		const user = new User({ email, username , role: 'admin'});
+		const registeredUser = await User.register(user, password);
 		req.login(registeredUser, (err) => {
 			if (err) return next(err);
 			req.flash('success', `Welcome ${username}`);
