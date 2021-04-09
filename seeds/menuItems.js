a7f732aa91e540c533c008f2f5b067c4bebc2568
Adam Nasser <adam.nasser246@gmail.com>
2021-04-09 14:19:46 +1000

    modified mock data

--- seeds/menuItems.js
+++ seeds/menuItems.js
@@ -1,11 +1,13 @@
 module.exports = [
     {
         name: 'Big Mac',
+        price: 4,
         description: `Mouthwatering perfection starts with two 100% pure beef patties and Big Mac® sauce sandwiched between a sesame seed bun. It's topped off with pickles, crisp shredded lettuce, finely chopped onion and American cheese for a 100% beef burger with a taste like no other.`,
         imageUrl: `https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Big-Mac.jpg`,
     },
     {
         name: 'Big Mac2',
+        price: 24,
         description: `Mouthwatering perfection starts with two 100% pure beef patties and Big Mac® sauce sandwiched between a sesame seed bun. It's topped off with pickles, crisp shredded lettuce, finely chopped onion and American cheese for a 100% beef burger with a taste like no other.`,
         imageUrl: `https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Big-Mac.jpg`,
     }
