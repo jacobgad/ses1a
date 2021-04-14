5ab2fbbc4eed266ef473bd06766821bd6e8175e5
Jacob Gad <jacob.gad@hotmail.com>
2021-04-14 17:36:53 +1000

    clean up code for merge

--- seeds/menuItems.js
+++ seeds/menuItems.js
@@ -1,16 +1,16 @@
 module.exports = [
-    {
-        name: 'Big Mac',
-        price: 4,
-        description: `Mouthwatering perfection starts with two 100% pure beef patties and Big Mac® sauce sandwiched between a sesame seed bun. It's topped off with pickles, crisp shredded lettuce, finely chopped onion and American cheese for a 100% beef burger with a taste like no other.`,
-        imageUrl: `https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Big-Mac.jpg`,
-        price: 9.95
-    },
-    {
-        name: 'Big Mac2',
-        price: 24,
-        description: `Mouthwatering perfection starts with two 100% pure beef patties and Big Mac® sauce sandwiched between a sesame seed bun. It's topped off with pickles, crisp shredded lettuce, finely chopped onion and American cheese for a 100% beef burger with a taste like no other.`,
-        imageUrl: `https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Big-Mac.jpg`,
-        price: 12.95
-    }
-]
+	{
+		name: 'Big Mac',
+		price: 4,
+		description: `Mouthwatering perfection starts with two 100% pure beef patties and Big Mac® sauce sandwiched between a sesame seed bun. It's topped off with pickles, crisp shredded lettuce, finely chopped onion and American cheese for a 100% beef burger with a taste like no other.`,
+		imageUrl: `https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Big-Mac.jpg`,
+		price: 9.95,
+	},
+	{
+		name: 'Big Mac2',
+		price: 24,
+		description: `Mouthwatering perfection starts with two 100% pure beef patties and Big Mac® sauce sandwiched between a sesame seed bun. It's topped off with pickles, crisp shredded lettuce, finely chopped onion and American cheese for a 100% beef burger with a taste like no other.`,
+		imageUrl: `https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Big-Mac.jpg`,
+		price: 12.95,
+	},
+];
