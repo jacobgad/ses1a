317c5640589392cb5d035a40e8d371b5659e91bf
Jacob Gad <jacob.gad@hotmail.com>
2021-05-05 19:17:44 +1000

    convert username to all lowercase on registration and login

--- routes/users.js
+++ routes/users.js
@@ -2,31 +2,28 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-const { isLoggedIn, isUser } = require('../middleware/users');
+const { isLoggedIn, isUser, usernameToLowerCase } = require('../middleware/users');
 const users = require('../controllers/users');
 
-router.route('/register')
-	.get(users.renderRegister)
-	.post(users.register);
+router.route('/register').get(users.renderRegister).post(users.register);
 
-router.route('/login')
+router
+	.route('/login')
 	.get(users.renderLogin)
-	.post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+	.post(
+		usernameToLowerCase,
+		passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+		users.login
+	);
 
-router.get('/logout', users.logout)
+router.get('/logout', users.logout);
 
-router.route('/users/:username')
-	.put(isLoggedIn, isUser, users.updateUser)
-	.delete(isLoggedIn, isUser, users.deleteUser)
+router.route('/users/:username').put(isLoggedIn, isUser, users.updateUser).delete(isLoggedIn, isUser, users.deleteUser);
 
-router.get('/users/:username/edit', isLoggedIn, isUser, users.renderEdit)
+router.get('/users/:username/edit', isLoggedIn, isUser, users.renderEdit);
 
-router.route('/forgot')
-	.get(users.renderForgot)
-	.post(users.forgot)
+router.route('/forgot').get(users.renderForgot).post(users.forgot);
 
-router.route('/reset/:token')
-	.get(users.renderReset)
-	.post(users.reset)
+router.route('/reset/:token').get(users.renderReset).post(users.reset);
 
 module.exports = router;
