fb53170f90c2efc32043384caeaf25d3105d080b
Jacob Gad <jacob.gad@hotmail.com>
2021-05-05 23:44:32 +1000

    remove image from cloudinary when deleting

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,4 +1,5 @@
 const Menu = require('../models/menuItems');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.renderMenu = async (req, res) => {
 	const menu = await Menu.find({});
@@ -6,15 +7,15 @@ module.exports.renderMenu = async (req, res) => {
 };
 
 module.exports.renderNew = (req, res) => {
-	res.render('menu/new', {page: 'menu'});
+	res.render('menu/new', { page: 'menu' });
 };
 
 module.exports.createMenuItem = async (req, res) => {
 	try {
 		const menuItem = new Menu(req.body.menuItem);
-		if (req.body.file) menuItem.image = { url: req.file.path, filename: req.file.filename };
+		if (req.file) menuItem.image = { url: req.file.path, filename: req.file.filename };
 		await menuItem.save();
-		res.redirect('/admin');
+		res.redirect('/admin/menu');
 	} catch (e) {
 		req.flash('error', e.message);
 		res.redirect('back');
@@ -23,7 +24,8 @@ module.exports.createMenuItem = async (req, res) => {
 
 module.exports.deleteMenuItem = async (req, res) => {
 	const { id } = req.body;
-	await Menu.findByIdAndDelete(id);
+	const menuItem = await Menu.findByIdAndDelete(id);
+	if (menuItem.image) await cloudinary.uploader.destroy(menuItem.image.filename);
 	req.flash('success', 'Successfully deleted account');
 	res.redirect('/admin/menu');
 };
